b08de5a734c5f2591cb7a858c052c2d18dba99ac
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-07 18:42:05 +0900

    feat: CreateChannelPasswordDto 검증 기능 추가
    - password 속성이 최대 72 바이트를 넘기지 않는지 검증하는 기능 추가
    - password 속성 관련 에러 메시지 출력 시 문자열이어야 함을 명시하기 위해 @IsString() 데코레이터 추가

--- backend/application/src/channel-passwords/dto/create-channel-password.dto.ts
+++ backend/application/src/channel-passwords/dto/create-channel-password.dto.ts
@@ -1,9 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsPositive, IsString, Length } from 'class-validator';
+import {
+  IsByteLength,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
 
 export class CreateChannelPasswordDto {
+  @IsByteLength(0, 72) // for bcrypt
   @Length(4, 20)
-  @IsString()
+  @IsString() // 사실상 필요 없지만 에러 메시지 출력에서 문자열임을 표현하려면 있는 게 좋다
   @IsNotEmpty()
   @ApiProperty({ minLength: 4, maxLength: 20 })
   password: string;
