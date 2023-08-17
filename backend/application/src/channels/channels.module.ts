dde36d60a1aa74e80e7a25c0d4d3931254561786
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-18 02:10:34 +0900

    feat: 채널 검색 시 입력한 문자열을 포함하는 채널들을 모두 표시하는 기능 구현
    + ChannelsController에서 ParticipantsService를 사용하도록 의존성을 설정하고, 채널의 참여자를 모두 찾을 때 주입된 participantsService를 이용하게 구현

--- backend/application/src/channels/channels.module.ts
+++ backend/application/src/channels/channels.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChannelsService } from './channels.service';
 import { ChannelsController } from './channels.controller';
 import { PrismaModule } from 'src/common';
+import { ParticipantsService } from 'src/participants/participants.service';
 
 @Module({
-  controllers: [ChannelsController],
-  providers: [ChannelsService],
-  imports: [PrismaModule],
+	controllers: [ChannelsController],
+	providers: [ChannelsService, ParticipantsService],
+	imports: [PrismaModule],
 })
 export class ChannelsModule { }
--- backend/application/src/participants/participants.service.ts
+++ backend/application/src/participants/participants.service.ts
@@ -4,28 +4,35 @@ import { PrismaService } from 'src/common';
 
 @Injectable()
 export class ParticipantsService {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) { }
 
-  async create(createParticipantDto: CreateParticipantDto) {
-    return await this.prisma.participant.create({ data: createParticipantDto });
-  }
+	async create(createParticipantDto: CreateParticipantDto) {
+		return await this.prisma.participant.create({ data: createParticipantDto });
+	}
 
-  async findAll() {
-    return await this.prisma.participant.findMany();
-  }
+	async findAll() {
+		return await this.prisma.participant.findMany();
+	}
 
-  async findOne(id: number) {
-    return await this.prisma.participant.findUniqueOrThrow({ where: { id } });
-  }
+	async findOne(id: number) {
+		return await this.prisma.participant.findUniqueOrThrow({ where: { id } });
+	}
 
-  async update(id: number, updateParticipantDto: UpdateParticipantDto) {
-    return await this.prisma.participant.update({
-      where: { id },
-      data: updateParticipantDto,
-    });
-  }
+	async findManyByChannelId(channelId: number) {
+		return await this.prisma.participant.findMany({
+			where: { channelId: channelId },
+			select: { user: { select: { nickname: true, avatar: true } } }
+		});
+	}
 
-  async remove(id: number) {
-    return await this.prisma.participant.delete({ where: { id } });
-  }
+	async update(id: number, updateParticipantDto: UpdateParticipantDto) {
+		return await this.prisma.participant.update({
+			where: { id },
+			data: updateParticipantDto,
+		});
+	}
+
+	async remove(id: number) {
+		return await this.prisma.participant.delete({ where: { id } });
+	}
 }
