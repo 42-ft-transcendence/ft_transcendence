05d403c325604f0b74feac662b6bc06a7269ff79
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-24 01:20:08 +0900

    feat: 현재 사용자가 채널에 참여 시, participant 모델을 활용하되 새로 생성한 channel 데이터를 반환하도록 구현

--- backend/application/src/participants/participants.service.ts
+++ backend/application/src/participants/participants.service.ts
@@ -4,33 +4,32 @@ import { PrismaService } from 'src/common';
 
 @Injectable()
 export class ParticipantsService {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) { }
 
-  async create(userId: number, createParticipantDto: CreateParticipantDto) {
-    return await this.prisma.participant.create({
-      data: {
-        userId: userId,
-        ...createParticipantDto,
-      },
-    });
-  }
+	async create(userId: number, createParticipantDto: CreateParticipantDto) {
+		const result = await this.prisma.participant.create({
+			data: { userId: userId, ...createParticipantDto },
+			select: { channel: true }
+		});
+		return result.channel;
+	}
 
-  async findAll() {
-    return await this.prisma.participant.findMany();
-  }
+	async findAll() {
+		return await this.prisma.participant.findMany();
+	}
 
-  async findOne(id: number) {
-    return await this.prisma.participant.findUniqueOrThrow({ where: { id } });
-  }
+	async findOne(id: number) {
+		return await this.prisma.participant.findUniqueOrThrow({ where: { id } });
+	}
 
-  async update(id: number, updateParticipantDto: UpdateParticipantDto) {
-    return await this.prisma.participant.update({
-      where: { id },
-      data: updateParticipantDto,
-    });
-  }
+	async update(id: number, updateParticipantDto: UpdateParticipantDto) {
+		return await this.prisma.participant.update({
+			where: { id },
+			data: updateParticipantDto,
+		});
+	}
 
-  async remove(id: number) {
-    return await this.prisma.participant.delete({ where: { id } });
-  }
+	async remove(id: number) {
+		return await this.prisma.participant.delete({ where: { id } });
+	}
 }
