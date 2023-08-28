c3d6310f361830918203ffb40387fb61485249c9
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-28 20:36:13 +0900

    refactor: 사용자가 최초 로그인 시 닉네임, 아바타를 업데이트하는 핸들러 경로 수정

--- backend/application/src/users/users.controller.ts
+++ backend/application/src/users/users.controller.ts
@@ -1,16 +1,16 @@
 import {
-	Controller,
-	Get,
-	Post,
-	Body,
-	Patch,
-	Param,
-	Delete,
-	UseGuards,
-	Query,
-	UseInterceptors,
-	UploadedFile,
-	ParseFilePipeBuilder,
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Query,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipeBuilder,
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
@@ -25,67 +25,74 @@ import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
 @Controller('users')
 @ApiTags('users')
 export class UsersController {
-	constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
-	@Post()
-	@UseGuards(JwtAuthGuard)
-	@ApiCreatedResponse({ type: UserEntity }) //TODO: ApiCreatedResponse는 생성(Post method)에 성공했을 때를 나타내는 것이다. 다음 링크를 참고해서 수정하자. 모든 컨트롤러를 다 수정하자. https://docs.nestjs.com/openapi/operations
-	async create(@Body() createUserDto: CreateUserDto) {
-		return await this.usersService.create(createUserDto);
-	}
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiCreatedResponse({ type: UserEntity }) //TODO: ApiCreatedResponse는 생성(Post method)에 성공했을 때를 나타내는 것이다. 다음 링크를 참고해서 수정하자. 모든 컨트롤러를 다 수정하자. https://docs.nestjs.com/openapi/operations
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
+  }
 
-	@Get()
-	@UseGuards(JwtAuthGuard)
-	@ApiOkResponse({ type: UserEntity, isArray: true })
-	async findAll(@CurrentUser(UserPropertyString.ID) id: number) {
-		return await this.usersService.findAll(id);
-	}
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: UserEntity, isArray: true })
+  async findAll(@CurrentUser(UserPropertyString.ID) id: number) {
+    return await this.usersService.findAll(id);
+  }
 
-	@Get('name')
-	@UseGuards(JwtAuthGuard)
-	@ApiOkResponse({ type: UserEntity, isArray: true })
-	async findByName(@CurrentUser(UserPropertyString.ID) id: number, @Query('name') name: string) {
-		return await this.usersService.findByName(id, name);
-	}
+  @Get('name')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: UserEntity, isArray: true })
+  async findByName(
+    @CurrentUser(UserPropertyString.ID) id: number,
+    @Query('name') name: string,
+  ) {
+    return await this.usersService.findByName(id, name);
+  }
 
-	@Patch('signup')
-	@UseInterceptors(FileInterceptor('file'))
-	@UseGuards(JwtAuthGuard)
-	@ApiCreatedResponse({ type: UserEntity })
-	async updateProfile(
-		@CurrentUser(UserPropertyString.ID) id: number,
-		@Body() updateUserProfileDto: UpdateUserProfileDto,
-		@UploadedFile(
-			new ParseFilePipeBuilder()
-				.addMaxSizeValidator({ maxSize: 1000000 })
-				.addFileTypeValidator({ fileType: 'image/jpeg' }) //TODO: 단순히 파일의 확장자를 확인할 뿐, 파일의 내용을 확인하진 않으므로 직접 magic number 등을 확인하게 구현해서 사용하자. 파일 타입도 image/png 등 추가하기
-				.build()
-		) file: Express.Multer.File,
-	) {
-		return await this.usersService.update(id, { ...updateUserProfileDto, avatar: file.path });
-	}
+  @Patch('update')
+  @UseInterceptors(FileInterceptor('file'))
+  @UseGuards(JwtAuthGuard)
+  @ApiCreatedResponse({ type: UserEntity })
+  async updateProfile(
+    @CurrentUser(UserPropertyString.ID) id: number,
+    @Body() updateUserProfileDto: UpdateUserProfileDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addMaxSizeValidator({ maxSize: 1000000 })
+        .addFileTypeValidator({ fileType: 'image/jpeg' }) //TODO: 단순히 파일의 확장자를 확인할 뿐, 파일의 내용을 확인하진 않으므로 직접 magic number 등을 확인하게 구현해서 사용하자. 파일 타입도 image/png 등 추가하기
+        .build(),
+    )
+    file: Express.Multer.File,
+  ) {
+    return await this.usersService.update(id, {
+      ...updateUserProfileDto,
+      avatar: file.path,
+    });
+  }
 
-	@Get(':id')
-	@UseGuards(JwtAuthGuard)
-	@ApiOkResponse({ type: UserEntity })
-	async findOne(@Param('id', ParsePositiveIntPipe) id: number) {
-		return await this.usersService.findOne(id);
-	}
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: UserEntity })
+  async findOne(@Param('id', ParsePositiveIntPipe) id: number) {
+    return await this.usersService.findOne(id);
+  }
 
-	@Patch(':id')
-	@UseGuards(JwtAuthGuard)
-	@ApiCreatedResponse({ type: UserEntity })
-	async update(
-		@Param('id', ParsePositiveIntPipe) id: number,
-		@Body() updateUserDto: UpdateUserDto,
-	) {
-		return await this.usersService.update(id, updateUserDto);
-	}
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiCreatedResponse({ type: UserEntity })
+  async update(
+    @Param('id', ParsePositiveIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.usersService.update(id, updateUserDto);
+  }
 
-	@Delete(':id')
-	@UseGuards(JwtAuthGuard)
-	@ApiOkResponse({ type: UserEntity })
-	async remove(@Param('id', ParsePositiveIntPipe) id: number) {
-		return await this.usersService.remove(id);
-	}
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: UserEntity })
+  async remove(@Param('id', ParsePositiveIntPipe) id: number) {
+    return await this.usersService.remove(id);
+  }
 }
