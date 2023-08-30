c7c75fbfc94810c6d27daafc9f3f611fe28f504f
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-30 19:43:50 +0900

    style: prettier 코드 스타일 정의 및 적용

--- backend/application/src/common/guard/channel-admin/channel-admin.guard.ts
+++ backend/application/src/common/guard/channel-admin/channel-admin.guard.ts
@@ -1,8 +1,8 @@
 import {
-  CanActivate,
-  ExecutionContext,
-  Inject,
-  Injectable,
+	CanActivate,
+	ExecutionContext,
+	Inject,
+	Injectable,
 } from '@nestjs/common';
 import { ChannelType } from '@prisma/client';
 import { Request } from 'express';
@@ -10,17 +10,19 @@ import { PrismaService } from 'src/common/prisma';
 
 @Injectable()
 export class ChannelAdminGuard implements CanActivate {
-  constructor(@Inject(PrismaService) private prisma: PrismaService) { }
+	constructor(@Inject(PrismaService) private prisma: PrismaService) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest<Request>();
-    const userId = parseInt(request.user['id']);
-    const channelId = parseInt(request.header('transcendence-authorization'));
-    const result = await this.prisma.administrator.findUnique({
-      where: { channelId_userId: { channelId: channelId, userId: userId } },
-      select: { channel: { select: { type: true } } }
-    });
-    if (result && result.channel.type !== ChannelType.ONETOONE) return true;
-    return false;
-  }
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest<Request>();
+		const userId = parseInt(request.user['id']);
+		const channelId = parseInt(
+			request.header('transcendence-authorization-channel-id'),
+		);
+		const result = await this.prisma.administrator.findUnique({
+			where: { channelId_userId: { channelId: channelId, userId: userId } },
+			select: { channel: { select: { type: true } } },
+		});
+		if (result && result.channel.type !== ChannelType.ONETOONE) return true;
+		return false;
+	}
 }
