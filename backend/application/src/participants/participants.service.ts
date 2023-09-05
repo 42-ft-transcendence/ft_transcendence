c1f5d792373f9267848c5ae733eac47153df4d27
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-09-06 03:35:13 +0900

    fix: 코드 수정
    - 사용자가 소유자일 때 대상 채널을 나가도 관리자 정보에 남아있는 오류 수정

--- backend/application/src/participants/participants.service.ts
+++ backend/application/src/participants/participants.service.ts
@@ -51,7 +51,6 @@ export class ParticipantsService {
 					name: true,
 					ownerId: true,
 					administrators: {
-						where: { NOT: { userId: userId } },
 						select: { userId: true },
 						orderBy: { id: 'asc' },
 					},
@@ -78,7 +77,7 @@ export class ParticipantsService {
 			if (channel.ownerId !== userId) return { id: channel.id };
 
 			// 관리자가 있다면 관리자 중 가장 먼저 관리자가 된 사람에게 소유권을 넘긴다. 관리자가 없다면 일반 참여자 중 가장 먼저 참여한 사람에게 소유권을 넘긴다. 참여자가 아예 없다면 채널을 제거한다.
-			const firstAdministrator = channel.administrators.at(0);
+			const firstAdministrator = channel.administrators.at(1); // 여기선 무조건 현재 사용자는 채널 소유자이므로, 0번째 인덱스 관리자는 현재 사용자고, 1번째 인덱스 관리자부터 다른 사용자다. 소유자는 관리자 중 가장 먼저 관리자로 지정된 사람이니까!
 			const firstParticipant = channel.participants.at(0);
 
 			if (firstAdministrator) {
