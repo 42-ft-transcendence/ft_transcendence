cf13486a013ff10d66af5ca6c4f7e7b160956093
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-09-08 17:14:45 +0900

    refactor: 업로드한 파일명에 사용자 닉네임이 포함되도록 변경

--- backend/application/src/users/users.module.ts
+++ backend/application/src/users/users.module.ts
@@ -25,7 +25,13 @@ import { AuthModule } from 'src/auth/auth.module';
 					filename(req, file, callback) {
 						// Multer는 FileInterceptor로 적용이 되고, 인터셉터는 Guard보다 나중에 적용되므로 FileInterceptor가 적용되는 시점엔 req.user에 id 속성은 반드시 존재한다.
 						// id 속성이 존재하지 않는다면, 이는 JWT가 유효하지 않다는 말이므로 애초에 JwtAuthGuard에서 예외를 던져 이 코드가 실행되지 않는다.
-						callback(null, req.user['id'] + '.jpeg'); //TODO: 파일 확장자 추가?
+						//TODO: 나중에 파일 GET 요청을 위한 api 구현 시 경로 수정하기
+						callback(
+							null,
+							`${req.user['nickname']}
+							_avatar.
+							${file.mimetype.split('image/').at(1)}`,
+						); //TODO: 파일 확장자 추가?
 					},
 				}),
 			}),
