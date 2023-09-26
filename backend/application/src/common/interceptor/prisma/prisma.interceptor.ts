93e5472a92c08e4d03cdb948691e446576ddaf2a
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-09-26 22:07:44 +0900

    fix: class-validator로 인한 에러가 PrismaInterceptor에 걸려 500번 에러로 반환되는 문제 수정
    - 그런데 type annotation을 이용해서 err의 타입을 Prisma.PrismaClientKnownRequestError 으로 지정했는데 왜 걸리지..?

--- backend/application/src/common/interceptor/prisma/prisma.interceptor.ts
+++ backend/application/src/common/interceptor/prisma/prisma.interceptor.ts
@@ -13,7 +13,7 @@ export class PrismaInterceptor implements NestInterceptor {
 		return next.handle().pipe(
 			tap({
 				error: (err: Prisma.PrismaClientKnownRequestError) => {
-					switch ((err.meta?.target as Array<string>).at(0)) {
+					switch ((err.meta?.target as Array<string>)?.at(0)) {
 						case 'nickname':
 							err.meta.target = '닉네임';
 							throw err;
